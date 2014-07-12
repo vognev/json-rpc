a76c267a69c1c99639ee66b2b245987133b0497a
Kilte Leichnam <nwotnbm@gmail.com>
2014-07-12 23:59:41 +0400

    Updated specification test

--- src/Kilte/JsonRpc/Tests/SpecificationTest.php
+++ src/Kilte/JsonRpc/Tests/SpecificationTest.php
@@ -22,22 +22,45 @@ class SpecificationTest extends \PHPUnit_Framework_TestCase
      */
     public function testSpecification()
     {
-        $this->markTestSkipped('Batch not implemented');
         $app = [];
         $app['subtract'] = function ($a, $b) {
             return $a - $b;
         };
         $app['update'] = function ($a, $b, $c, $e, $d) {
-            return $a + $b + $c + $e + $d;
         };
         $app['baz'] = function () {
-            return null;
+        };
+        $app['notify_sum'] = function ($a, $b, $c) {
+        };
+        $app['notify_hello'] = function ($a) {
+        };
+        $app['sum'] = function ($a, $b, $c) {
+            return $a + $b + $c;
+        };
+        $app['get_data'] = function () {
+            return ['hello', 5];
         };
         $ioStream = new IOStreamFactory();
         $response = new ResponseFactory();
         $response->add('http', '\\Kilte\\JsonRpc\\Response\\HttpResponse');
         $server = new Server(new Application($app), $ioStream, $response, 'http');
         $streamPath = __DIR__ . '/Fixtures/Specification/%s';
+        $invalidRequest = [
+            'jsonrpc' => '2.0',
+            'error'   => [
+                'code'    => -32600,
+                'message' => 'The JSON sent is not a valid Request object.'
+            ],
+            'id'      => null
+        ];
+        $parseError = [
+            'jsonrpc' => '2.0',
+            'error'   => [
+                'code'    => -32700,
+                'message' => 'An error occurred on the server while parsing the JSON text.'
+            ],
+            'id'      => null
+        ];
         $fixtures = [
             '01_positional_params.json'           => ['jsonrpc' => '2.0', 'result' => 19, 'id' => 1],
             '02_named_params.json'                => ['jsonrpc' => '2.0', 'result' => -19, 'id' => 3],
@@ -50,54 +73,65 @@ class SpecificationTest extends \PHPUnit_Framework_TestCase
                 ],
                 'id'      => 1
             ],
-            '05_invalid_json.txt'                 => [
-                'jsonrpc' => '2.0',
-                'error'   => [
-                    'code'    => -32700,
-                    'message' => 'An error occurred on the server while parsing the JSON text.'
-                ],
-                'id'      => null
-            ],
-            '06_invalid_request_object.json'      => [
-                'jsonrpc' => '2.0',
-                'error'   => [
-                    'code'    => -32600,
-                    'message' => 'The JSON sent is not a valid Request object.'
-                ],
-                'id'      => null
-            ],
-            '07_empty_array.json'                 => [
-                'jsonrpc' => '2.0',
-                'error'   => [
-                    'code'    => -32600,
-                    'message' => 'The JSON sent is not a valid Request object.'
-                ],
-                'id'      => null
-            ],
+            '05_invalid_json.txt'                 => $parseError,
+            '06_invalid_request_object.json'      => $invalidRequest,
+            '07_empty_array.json'                 => $invalidRequest,
             '08_notification_without_params.json' => '',
             '09_batch_notifications.json'         => '',
             '10_batch.json'                       => [
                 ['jsonrpc' => '2.0', 'result' => 7, 'id' => '1'],
                 ['jsonrpc' => '2.0', 'result' => 19, 'id' => '2'],
-                ['jsonrpc' => '2.0', 'error' => ['code' => -32600, 'message' => 'Invalid Request'], 'id' => null],
-                ['jsonrpc' => '2.0', 'error' => ['code' => -32601, 'message' => 'Method not found'], 'id' => '5'],
+                [
+                    'jsonrpc' => '2.0',
+                    'error'   => [
+                        'code'    => -32600,
+                        'message' => 'The JSON sent is not a valid Request object.'
+                    ],
+                    'id'      => null
+                ],
+                [
+                    'jsonrpc' => '2.0',
+                    'error'   => ['code' => -32601, 'message' => 'Method "foo.get" does not exists'],
+                    'id'      => '5'
+                ],
                 ['jsonrpc' => '2.0', 'result' => ['hello', 5], 'id' => '9']
             ],
-            '11_invalid_json_batch.txt'           => [
-                'jsonrpc' => '2.0',
-                'error'   => [
-                    'code'    => -32700,
-                    'message' => 'Parse error'
+            '11_invalid_json_batch.txt'           => $parseError,
+            '12_invalid_batch.json'               => [
+                [
+                    'jsonrpc' => '2.0',
+                    'error'   => [
+                        'code'    => -32600,
+                        'message' => 'The JSON sent is not a valid Request object.'
+                    ],
+                    'id'      => null
                 ],
-                'id'      => null
+                [
+                    'jsonrpc' => '2.0',
+                    'error'   => [
+                        'code'    => -32600,
+                        'message' => 'The JSON sent is not a valid Request object.'
+                    ],
+                    'id'      => null
+                ],
+                [
+                    'jsonrpc' => '2.0',
+                    'error'   => [
+                        'code'    => -32600,
+                        'message' => 'The JSON sent is not a valid Request object.'
+                    ],
+                    'id'      => null
+                ],
             ],
-            '12_invalid_batch.json' => [
-                ['jsonrpc' => '2.0', 'error' => ['code' => -32600, 'message' => 'Invalid Request'], 'id' => null],
-                ['jsonrpc' => '2.0', 'error' => ['code' => -32600, 'message' => 'Invalid Request'], 'id' => null],
-                ['jsonrpc' => '2.0', 'error' => ['code' => -32600, 'message' => 'Invalid Request'], 'id' => null],
-            ],
-            '12_not_empty_invalid_batch.json' => [
-                ['jsonrpc' => '2.0', 'error' => ['code' => -32600, 'message' => 'Invalid Request'], 'id' => null],
+            '13_not_empty_invalid_batch.json'     => [
+                [
+                    'jsonrpc' => '2.0',
+                    'error'   => [
+                        'code'    => -32600,
+                        'message' => 'The JSON sent is not a valid Request object.'
+                    ],
+                    'id'      => null
+                ],
             ]
         ];
         foreach ($fixtures as $request => $expectedResponse) {
