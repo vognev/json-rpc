062e9e334011be61af74c7c3c3bbbb508a7b206d
Kilte Leichnam <nwotnbm@gmail.com>
2014-07-12 22:27:43 +0400

    Use StdClass instead array

--- src/Kilte/JsonRpc/Request/AbstractFactory.php
+++ src/Kilte/JsonRpc/Request/AbstractFactory.php
@@ -32,22 +32,22 @@ abstract class AbstractFactory
     /**
      * Validates a request data
      *
-     * @param array $request Request data
+     * @param \StdClass $request Request data
      *
      * @return boolean
      */
-    private function validateRequest(array $request)
+    private function validateRequest(\StdClass $request)
     {
         $isValid = true;
         foreach (['jsonrpc', 'method'] as $required) {
-            if (!array_key_exists($required, $request)) {
+            if (!isset($request->$required)) {
                 $isValid = false;
             }
         }
-        if (array_key_exists('params', $request) && !is_array($request['params'])) {
+        if (isset($request->params) && !is_array($request->params)) {
             $isValid = false;
         }
-        if (isset($request['jsonrpc']) && $request['jsonrpc'] !== Request::JSONRPC) {
+        if (isset($request->jsonrpc) && $request->jsonrpc !== Request::JSONRPC) {
             $isValid = false;
         }
 
@@ -63,18 +63,18 @@ abstract class AbstractFactory
      */
     public function forge()
     {
-        $request = json_decode($this->getRequest(), true);
-        if (!is_array($request)) {
+        $request = json_decode($this->getRequest(), false);
+        if (!$request instanceof \StdClass) {
             throw new ParseException();
         }
         if (!$this->validateRequest($request)) {
             throw new InvalidRequestException();
         }
         foreach (['id' => false, 'params' => []] as $notRequired => $value) {
-            $request[$notRequired] = array_key_exists($notRequired, $request) ? $request[$notRequired] : $value;
+            $request->$notRequired = isset($request->$notRequired) ? $request->$notRequired : $value;
         }
 
-        return new Request($request['method'], $request['params'], $request['id']);
+        return new Request($request->method, $request->params, $request->id);
     }
 
 }
